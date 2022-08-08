aa9adf3e19178ea06dd5a3dd909e2111eefa7749
Aline-Fernandez <alinefernandeslima@icloud.com>
2022-08-07 23:40:00 -0300

    Initial commit

---
+++ src/dio/digital/inovation/basecamp/Main.java
@@ -0,0 +1,14 @@
+package dio.digital.inovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Quadrilátero
+        System.out.println("Exercicio quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+    }
+}
